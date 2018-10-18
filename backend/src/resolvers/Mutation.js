fd9865053843e26e806551693c47538acf5b8b68
Ben Booth <bkbooth@gmail.com>
2018-10-18 22:45:51 +1100

    Create and get items from prisma

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,14 @@
 const Mutation = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    // create a dog
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args,
+      },
+    }, info)
+
+    return item
   },
 }
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,7 @@
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    return global.dogs
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items()
+    return items
   },
 }
 

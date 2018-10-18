6895bec375e5886a6a7b063fe21e3bdaad9a41d1
Ben Booth <bkbooth@gmail.com>
2018-10-18 22:17:26 +1100

    Create and fetch in-memory dogs

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutation = {}
+const Mutation = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || []
+    // create a dog
+    const newDog = { name: args.name }
+    global.dogs.push(newDog)
+    return newDog
+  },
+}
 
 module.exports = Mutation

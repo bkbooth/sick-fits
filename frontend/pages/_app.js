db67ddc5261c19ac13b7783db310cd707ebd5778
Ben Booth <bkbooth@gmail.com>
2018-10-18 23:14:21 +1100

    Connect Next.js app to Apollo client for state

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,32 @@
 import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
+import withData from '../lib/withData'
 import Page from '../components/Page'
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
+
   render() {
-    const { Component } = this.props
+    const { apollo, Component, pageProps } = this.props
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp
+export default withData(MyApp)

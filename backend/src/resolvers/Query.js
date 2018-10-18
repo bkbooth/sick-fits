2e93e2155663d27c0dae316e48459f6d7843b985
Ben Booth <bkbooth@gmail.com>
2018-10-18 22:48:30 +1100

    Use prisma binding to forward query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,8 +1,12 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items()
-    return items
-  },
+  items: forwardTo('db'),
+
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // },
 }
 
 module.exports = Query

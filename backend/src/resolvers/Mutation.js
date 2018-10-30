d7b41018132ddf46a10f74642a900446e5185e02
Ben Booth <bkbooth@gmail.com>
2018-10-30 22:47:18 +1100

    Add mutation and component to delete items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -22,6 +22,16 @@ const Mutation = {
       where: { id: args.id },
     }, info)
   },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`)
+    // 2. check if the user owns the item, or has permissions
+    // TODO
+    // 3. delete it!
+    return ctx.db.mutation.deleteItem({ where }, info)
+  },
 }
 
 module.exports = Mutation
--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Title from './styles/Title'
 import ItemStyles from './styles/ItemStyles'
 import PriceTag from './styles/PriceTag'
+import DeleteItem from './DeleteItem'
 import formatMoney from '../lib/formatMoney'
 
 class Item extends Component {
@@ -29,7 +30,7 @@ class Item extends Component {
           <a>Edit ✏️</a>
         </Link>
         <button>Add to Cart</button>
-        <button>Delete</button>
+        <DeleteItem id={item.id}>Delete This Item</DeleteItem>
       </div>
     </ItemStyles>
   }

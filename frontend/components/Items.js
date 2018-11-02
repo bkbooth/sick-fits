f916686106ac6a7f04dec117fb62164d36d08fe9
Ben Booth <bkbooth@gmail.com>
2018-11-02 22:59:16 +1100

    Items lists use pagination params

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,10 +4,11 @@ import gql from 'graphql-tag'
 import styled from 'styled-components'
 import Item from './Item'
 import Pagination from './Pagination'
+import { perPage } from '../config'
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(skip: $skip, first: $first, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -35,7 +36,9 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query query={ALL_ITEMS_QUERY} variables={{
+          skip: this.props.page * perPage - perPage,
+        }}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>

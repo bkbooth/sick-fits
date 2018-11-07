ee3922b23372286cff36c5abcc1265fce711546d
Ben Booth <bkbooth@gmail.com>
2018-11-07 22:04:29 +1100

    Add query and component to get the current user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
@@ -8,7 +9,16 @@ const server = createServer()
 
 server.express.use(cookieParser())
 
-// TODO: use express middleware to populate current user
+// decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    // put the user ID onto the req for the remainder of the request
+    req.userId = userId
+  }
+  next()
+})
 
 server.start({
   cors: {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -5,10 +5,13 @@ const Query = {
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
 
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items()
-  //   return items
-  // },
+  me(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) return null
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId },
+    }, info)
+  },
 }
 
 module.exports = Query

b49126cf1c363ae25da63840cba899d92e6e517d
Ben Booth <bkbooth@gmail.com>
2018-11-13 15:05:09 +1100

    Add middleware to populate current user on all requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,17 @@ server.express.use((req, res, next) => {
   next()
 })
 
+// populate the user on each request
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next()
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, email, name, permissions }',
+  )
+  req.user = user
+  next()
+})
+
 server.start({
   cors: {
     credentials: true,

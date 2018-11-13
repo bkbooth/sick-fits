5ab883ae9976870499bff60d85636be67fdceb91
Ben Booth <bkbooth@gmail.com>
2018-11-13 15:27:50 +1100

    Add a page and component to list all users and permissions

---
+++ frontend/components/Permissions.js
@@ -0,0 +1,71 @@
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import Error from './ErrorMessage'
+import Table from './styles/Table'
+import SickButton from './styles/SickButton'
+
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE',
+]
+
+const ALL_USERS_QUERY = gql`
+  query ALL_USERS_QUERY {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`
+
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => (
+      <div>
+        <Error error={error} />
+
+        <div>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
+                <th>👇</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.users.map(user => <User user={user} key={user.id} />)}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    )}
+  </Query>
+)
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user
+    return <tr>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      {possiblePermissions.map(permission => <td key={`${user.id}-${permission}`}>
+        <label htmlFor={`${user.id}-permission-${permission}`}>
+          <input type="checkbox" />
+        </label>
+      </td>)}
+      <td><SickButton>Update</SickButton></td>
+    </tr>
+  }
+}
+
+export default Permissions
+export { ALL_USERS_QUERY }
---
+++ frontend/pages/permissions.js
@@ -0,0 +1,12 @@
+import PleaseSignin from '../components/PleaseSignin'
+import Permissions from '../components/Permissions'
+
+const PermissionsPage = props => (
+  <div>
+    <PleaseSignin>
+      <Permissions />
+    </PleaseSignin>
+  </div>
+)
+
+export default PermissionsPage

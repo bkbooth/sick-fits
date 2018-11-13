082ffde4761d0b89ead8baa7457291828df8f9fe
Ben Booth <bkbooth@gmail.com>
2018-11-13 12:35:58 +1100

    Associate items with users

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,10 +7,16 @@ const { transport, makeANiceEmail } = require('../mail')
 
 const Mutation = {
   async createItem(parent, args, ctx, info) {
-    // TODO: check if they are logged in
+    if (!ctx.request.userId) throw new Error('You must be logged in to do that!')
 
     const item = await ctx.db.mutation.createItem({
       data: {
+        // This is how we create a relationship
+        user: {
+          connect: {
+            id: ctx.request.userId,
+          },
+        },
         ...args,
       },
     }, info)

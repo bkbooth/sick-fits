8d0f96cbf7226d28e585a91a4214f3076d9b434c
Ben Booth <bkbooth@gmail.com>
2018-11-15 09:09:59 +1100

    Add an autocomplete search component

---
+++ frontend/components/Search.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Downshift from 'downshift'
+import Router from 'next/router'
+import { ApolloConsumer } from 'react-apollo'
+import gql from 'graphql-tag'
+import debounce from 'lodash.debounce'
+import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown'
+
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(where: {
+      OR: [
+        { title_contains: $searchTerm },
+        { description_contains: $searchTerm },
+      ],
+    }) {
+      id
+      image
+      title
+    }
+  }
+`
+
+class AutoComplete extends React.Component {
+  state = {
+    items: [],
+    loading: false,
+  }
+
+  onChange = debounce(async (e, client) => {
+    // Turn loading on
+    this.setState({ loading: true })
+    // Manually query apollo client
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { searchTerm: e.target.value },
+    })
+    // Set returned items into state
+    this.setState({
+      items: res.data.items,
+      loading: false,
+    })
+  }, 350)
+
+  render() {
+    return <SearchStyles>
+      <div>
+        <ApolloConsumer>
+          {(client) => <input
+            type="search"
+            onChange={(e) => {
+              e.persist()
+              this.onChange(e, client)
+            }}
+          />}
+        </ApolloConsumer>
+        <DropDown>
+          {this.state.items.map(item => <DropDownItem key={item.id}>
+            <img src={item.image} alt={item.title} width="50" />
+            {item.title}
+          </DropDownItem>)}
+        </DropDown>
+      </div>
+    </SearchStyles>
+  }
+}
+
+export default AutoComplete
+export { SEARCH_ITEMS_QUERY }

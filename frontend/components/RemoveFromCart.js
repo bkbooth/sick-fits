d3128027b56bccf00087f224e0a1139a41e545a7
Ben Booth <bkbooth@gmail.com>
2018-11-14 16:38:49 +1100

    Immediately optimistically delete from cart

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,10 +28,33 @@ class RemoveFromCart extends React.Component {
     id: PropTypes.string.isRequired,
   }
 
+  // This gets called as soon as we get a response
+  // back from the server after the mutation has been performed
+  update = (cache, payload) => {
+    // 1. first read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY })
+    // 2. remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId)
+    // 3. write it back to the cache
+    cache.writeQuery({
+      query: CURRENT_USER_QUERY,
+      data,
+    })
+  }
+
   render() {
     return <Mutation
       mutation={REMOVE_FROM_CART_MUTATION}
       variables={{ id: this.props.id }}
+      update={this.update}
+      optimisticResponse={{
+        __typename: 'Mutation',
+        removeFromCart: {
+          __typename: 'CartItem',
+          id: this.props.id,
+        },
+      }}
     >
       {(removeFromCart, { loading }) => (
         <BigButton

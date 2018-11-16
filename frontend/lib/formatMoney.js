9b375eff48dc512e342659884bf9be99bb420ccc
Ben Booth <bkbooth@gmail.com>
2018-11-16 11:01:49 +1100

    Add unit tests for the formatMoney function

--- frontend/lib/formatMoney.js
+++ frontend/lib/formatMoney.js
@@ -6,6 +6,6 @@ export default function(amount) {
   }
   // if its a whole, dollar amount, leave off the .00
   if (amount % 100 === 0) options.minimumFractionDigits = 0
-  const formatter = new Intl.NumberFormat('en-US', options)
+  const formatter = new Intl.NumberFormat('en-AU', options)
   return formatter.format(amount / 100)
 }

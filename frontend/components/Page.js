5cb9034b2567fa0a728f1e95ca7fe6dd73a46667
Ben Booth <bkbooth@gmail.com>
2019-08-02 16:24:22 +1000

    Add a styled-components global stylesheet

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
@@ -12,6 +12,36 @@ const theme = {
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.black};
+  }
+  button {
+    font-family: 'radnika_next';
+  }
+`;
 
 const StyledPage = styled.div`
   background: white;
@@ -27,6 +57,7 @@ const Inner = styled.div`
 const Page = ({ children }) => (
   <ThemeProvider theme={theme}>
     <StyledPage>
+      <GlobalStyle />
       <Meta />
       <Header />
       <Inner>{children}</Inner>

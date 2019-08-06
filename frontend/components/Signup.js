83a2e340476cdaf3c83ab23cb7bc378477b5b768
Ben Booth <bkbooth@gmail.com>
2019-08-06 17:29:06 +1000

    Handle user signup on frontend

---
+++ frontend/components/Signup.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Error from 'components/ErrorMessage';
+import Form from 'components/styles/Form';
+
+export const SIGNUP_MUTATION = gql`
+  mutation SIGNUP($name: String!, $email: String!, $password: String!) {
+    signup(name: $name, email: $email, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function createChangeHandler(setter) {
+    return event => setter(event.currentTarget.value);
+  }
+
+  function createSubmissionHandler(mutation) {
+    return async event => {
+      event.preventDefault();
+      await mutation();
+
+      setName('');
+      setEmail('');
+      setPassword('');
+    };
+  }
+
+  return (
+    <Mutation mutation={SIGNUP_MUTATION} variables={{ name, email, password }}>
+      {(signup, { error, loading }) => (
+        <Form onSubmit={createSubmissionHandler(signup)} method="post">
+          <fieldset disabled={loading} aria-busy={loading}>
+            <h2>Signup for an account</h2>
+
+            <Error error={error} />
+
+            <label htmlFor="name">
+              Name
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Name"
+                value={name}
+                onChange={createChangeHandler(setName)}
+                required
+              />
+            </label>
+
+            <label htmlFor="email">
+              Email
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={createChangeHandler(setEmail)}
+                required
+              />
+            </label>
+
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={createChangeHandler(setPassword)}
+                required
+              />
+            </label>
+
+            <button type="submit">Sign{loading ? 'ing ' : ''}up</button>
+          </fieldset>
+        </Form>
+      )}
+    </Mutation>
+  );
+};
+
+export default Signup;

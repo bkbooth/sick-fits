88f23be4f2a985f52b709ddadf0b6c6b69cee789
Ben Booth <bkbooth@gmail.com>
2019-08-07 15:46:04 +1000

    Populate the current user on the express request

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,22 @@
 require('dotenv').config({ path: 'variables.env' });
+const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const createServer = require('./createServer');
+const db = require('./db');
 
-const server = require('./createServer')();
-server.express.use(require('cookie-parser')());
+const server = createServer();
+server.express.use(cookieParser());
 server.express.use((req, _res, next) => {
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     req.userId = userId;
   }
+  next();
+});
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+  req.user = await db.query.user({ where: { id: req.userId } }, '{ id, email, name, permissions }');
   next();
 });
 

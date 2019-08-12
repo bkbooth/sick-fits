a36faf998a8a168b3aceea6ac21c3e447ce95c28
Ben Booth <bkbooth@gmail.com>
2019-08-12 10:24:39 +1000

    Use the fakeItem from testUtils for Item tests

--- frontend/__tests__/Item.test.js
+++ frontend/__tests__/Item.test.js
@@ -1,36 +1,28 @@
 import { shallow } from 'enzyme';
+import { fakeItem } from 'lib/testUtils';
 import Item from 'components/Item';
-
-const fakeItem = {
-  id: 'abc123',
-  title: 'Testing item',
-  description: 'A really testable item',
-  price: 5000,
-  image: 'item.jpg',
-  largeImage: 'yugeitem.jpg',
-};
 
 describe('<Item />', () => {
   it('renders and matches the snapshot', () => {
-    const wrapper = shallow(<Item item={fakeItem} />);
+    const wrapper = shallow(<Item item={fakeItem()} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders the title and price tag', () => {
-    const wrapper = shallow(<Item item={fakeItem} />);
+    const wrapper = shallow(<Item item={fakeItem()} />);
     expect(wrapper.find('PriceTag').text()).toBe('$50');
-    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+    expect(wrapper.find('Title a').text()).toBe('Testing item');
   });
 
   it('renders the image', () => {
-    const wrapper = shallow(<Item item={fakeItem} />);
+    const wrapper = shallow(<Item item={fakeItem()} />);
     const image = wrapper.find('img');
-    expect(image.prop('src')).toBe(fakeItem.image);
-    expect(image.prop('alt')).toBe(fakeItem.title);
+    expect(image.prop('src')).toBe('item.jpg');
+    expect(image.prop('alt')).toBe('Testing item');
   });
 
   it('renders the buttoms', () => {
-    const wrapper = shallow(<Item item={fakeItem} />);
+    const wrapper = shallow(<Item item={fakeItem()} />);
     const buttonList = wrapper.find('.buttonList');
     expect(buttonList.children()).toHaveLength(3);
     expect(buttonList.find('Link').exists()).toBe(true);

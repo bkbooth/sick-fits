4e7fa264f8acd8398e693a75951b79df1f35fe67
Ben Booth <bkbooth@gmail.com>
2019-08-12 10:56:26 +1000

    Add tests for the PleaseSignin component

---
+++ frontend/__tests__/PleaseSignin.test.js
@@ -0,0 +1,52 @@
+import { MockedProvider } from 'react-apollo/test-utils';
+import { mount } from 'enzyme';
+import wait from 'waait';
+import { fakeUser } from 'lib/testUtils';
+import PleaseSignin from 'components/PleaseSignin';
+import { CURRENT_USER_QUERY } from 'components/User';
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } },
+  },
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } },
+  },
+];
+
+const ChildComponent = () => <p>I'm a child!</p>;
+
+describe('<PleaseSignin />', () => {
+  it('renders the sign in dialog when not signed in', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <PleaseSignin>
+          <ChildComponent />
+        </PleaseSignin>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain('Please sign in before continuing');
+    expect(wrapper.find('Signin').exists()).toBe(true);
+    expect(wrapper.contains(<ChildComponent />)).toBe(false);
+  });
+
+  it('renders the child component when signed in', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <PleaseSignin>
+          <ChildComponent />
+        </PleaseSignin>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.contains(<ChildComponent />)).toBe(true);
+  });
+});
--- frontend/lib/testUtils.js
+++ frontend/lib/testUtils.js
@@ -1,3 +1,8 @@
+import casual from 'casual';
+
+// seed it so we get consistent results
+casual.seed(777);
+
 export const fakeItem = () => ({
   __typename: 'Item',
   id: 'abc123',
@@ -7,4 +12,14 @@ export const fakeItem = () => ({
   image: 'item.jpg',
   largeImage: 'yugeitem.jpg',
   user: null,
+});
+
+export const fakeUser = () => ({
+  __typename: 'User',
+  id: casual.uuid,
+  name: casual.name,
+  email: casual.email,
+  permissions: ['ADMIN'],
+  orders: [],
+  cart: [],
 });

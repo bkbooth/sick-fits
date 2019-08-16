51ef00a5dffc1bc7c2f05c8c717d55e1ed23f41b
Ben Booth <bkbooth@gmail.com>
2019-08-16 23:05:59 +1000

    Fix failing Pagination tests

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,6 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+process.env.itemsPerPage = 4;
+
 global.alert = console.log;
 
 configure({ adapter: new Adapter() });
